ef2c5f185bf4c0ce4cffcda8af2079d9e9072e83
HODANGVUIT <hodangvu2003@gmail.com>
2024-11-25 13:56:58 +0700

    init project

---
+++ src/hoidanit/HelloWord.java
@@ -0,0 +1,9 @@
+package hoidanit;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("Xin chao");
+		System.out.println("Xin chao");
+		System.out.println("Xin chao");
+	}
+}
